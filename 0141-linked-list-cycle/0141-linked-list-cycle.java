92675156b0725debc36ca00bdf9888ea4d2ace52
Supriya shri <97748648+Supriya2903@users.noreply.github.com>
2025-09-01 02:05:08 +0530

    Time: 0 ms (100%), Space: 45.1 MB (6.79%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,18 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false; //If head is null or only one node with no next, no cycle.
+        ListNode slow = head;
+        ListNode fast = head;
 
-        ListNode slow = head; //Initialize Pointers
-        ListNode fast = head; //Initialize Pointers
-
-        while(fast != null && fast.next!= null){
-            slow = slow.next; //Move slow by 1, fast by 2.
+        while(fast != null && fast.next != null){
+            slow = slow.next;
             fast = fast.next.next;
 
             if(slow == fast){
                 return true;
             }
+
         }
         return false;
     }
